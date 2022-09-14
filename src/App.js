377856814dbec88f122141e762738112809bd061
nurhamsah dari rumah <nur.hamsah.cash@gmail.com>
2022-09-14 19:12:18 +0700

    belajar API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  };
+  React.useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data?.map((item, index) => (
+        <div key={index}>
+          <p> Name : {item?.name}</p>
+          <p> Email : {item?.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
